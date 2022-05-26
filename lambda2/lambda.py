dc18415c7400b6f7bcde2aef7e9ef7165ed9ab3a
Marco Silva <marco.antonio.almeida.silva@gmail.com>
2022-05-26 20:41:44 -0300

    added logging configurations

--- lambda2/lambda.py
+++ lambda2/lambda.py
@@ -1,3 +1,4 @@
+import os
 import logging
 
 import boto3
@@ -5,16 +6,11 @@ import boto3
 AWS_REGION = 'us-east-1'
 LOCALSTACK_INTERNAL_ENDPOINT_URL = 'http://host.docker.internal:4566'
 
-# logger config
 logger = logging.getLogger()
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s: %(levelname)s: %(message)s')
+logger.setLevel(logging.INFO)
 
 
 def get_boto3_client(service):
-    """
-    Initialize Boto3 client.
-    """
     try:
         boto3_client = boto3.client(
             service,
@@ -29,7 +25,11 @@ def get_boto3_client(service):
 
 def handler(event, context):
     s3_client = get_boto3_client('s3')
-    logging.info('Uploading an object to the localstack s3 bucket...')
+    logger.info('Uploading an object to the localstack s3 bucket...')
+    logger.info('logger level {}'.format(logger.getEffectiveLevel))
+    logger.info('## ENVIRONMENT VARIABLES ##\n{}'.format(os.environ))
+    logger.info('## EVENT ##\n{}'.format(event))
+
     object_key = 'hands-on-cloud'
     s3_client.put_object(
         Bucket='testbucket1',
